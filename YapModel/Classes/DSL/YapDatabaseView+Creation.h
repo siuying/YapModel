4db30387fe8f088d17c42cdc17c06ecf2fa579c8
Francis Chong <francis@ignition.hk>
2014-08-22 08:45:19 +0800

    add more tests

--- YapModel/Classes/DSL/YapDatabaseView+Creation.h
+++ YapModel/Classes/DSL/YapDatabaseView+Creation.h
@@ -10,6 +10,14 @@
 
 @interface YapDatabaseView (Creation)
 
+/**
+ * Create a view for a collection, use the supplied keys to group and sort.
+ * @param collection name of the collection
+ * @param groupByKeys [NSArray<String>] keys for properties used to group the view. the group name will be string joined by all values in order.
+ * @param sortByKeys [NSArray<String>] keys for properties used to sort the view.
+ * @param version version of the view
+ *
+ */
 +(instancetype) viewWithCollection:(NSString*)collection
                        groupByKeys:(NSArray*)groupByKeys
                         sortByKeys:(NSArray*)sortByKeys
--- YapModelTests/YapDatabaseView+CreationSpec.m
+++ YapModelTests/YapDatabaseView+CreationSpec.m
@@ -30,6 +30,7 @@ describe(@"YapDatabaseView+Creation", ^{
                 Person* person = [Person new];
                 person.name = [NSString stringWithFormat:@"Person%d", i];
                 person.age = 30 + i;
+                person.salary = 1000 * (i % 3);
                 [person saveWithTransaction:transaction];
             }
             Company* company = [Company new];
@@ -53,7 +54,7 @@ describe(@"YapDatabaseView+Creation", ^{
             connection = [database newConnection];
         });
 
-        it(@"should create a view with the specific model class", ^{
+        it(@"should create a view", ^{
             NSString* viewName = @"viewByPerson";
             YapDatabaseView* view = [YapDatabaseView viewWithCollection:[Person collectionName]
                                                             groupByKeys:@[@"age"]
@@ -66,12 +67,37 @@ describe(@"YapDatabaseView+Creation", ^{
             
             __block Person* person;
             [connection readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
+                [[[[transaction ext:viewName] allGroups] should] containObjects:@"30", nil];
                 person = [[transaction ext:viewName] objectAtIndex:0 inGroup:@"30"];
             }];
 
             [[person should] beNonNil];
             [[theValue(person.age) should] equal:theValue(30)];
             [[person.name should] equal:@"Person0"];
+            
+            [database unregisterExtension:viewName];
+        });
+        
+        it(@"should create a view with multiple sort by keys", ^{
+            NSString* viewName = @"viewByPerson2";
+            YapDatabaseView* view = [YapDatabaseView viewWithCollection:[Person collectionName]
+                                                            groupByKeys:@[@"salary"]
+                                                             sortByKeys:@[@"salary", @"name"]
+                                                                version:1];
+            BOOL registered = [database registerExtension:view withName:viewName];
+            [[theValue(registered) should] beTrue];
+            
+            CreateTestRecords(connection);
+            
+            __block Person* person;
+            [connection readWriteWithBlock:^(YapDatabaseReadWriteTransaction *transaction) {
+                person = [[transaction ext:viewName] objectAtIndex:0 inGroup:@"2000"];
+            }];
+            
+            [[person should] beNonNil];
+            [[theValue(person.age) should] equal:theValue(32)];
+            [[theValue(person.salary) should] equal:theValue(2000)];
+            [[person.name should] equal:@"Person2"];
             
             [database unregisterExtension:viewName];
         });
